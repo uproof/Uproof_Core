8cf1fe486132b0adbbab75a9048c3a4b8c7d40f0
MohsinMir-13 <115740411+MohsinMir-13@users.noreply.github.com>
2025-10-23 23:04:38 +0300

    chore(admin): remove unused icon import after logout refactor

--- app/[locale]/admin/page.tsx
+++ app/[locale]/admin/page.tsx
@@ -4,7 +4,6 @@ import {
   PencilSquareIcon,
   DocumentTextIcon,
   HomeIcon,
-  ArrowRightOnRectangleIcon,
   ChartBarIcon
 } from '@heroicons/react/24/outline';
 import LanguageSwitcher from '@/components/LanguageSwitcher';
